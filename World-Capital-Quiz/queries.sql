726d93b1549ab8bc134b33716a269c7c89710ea8
Vighnesh Kunjir <130839850+VighneshKunjir@users.noreply.github.com>
2024-06-02 19:30:16 +0530

    Add files via upload

---
+++ World-Capital-Quiz/queries.sql
@@ -0,0 +1,5 @@
+create table capitals(
+id serial primary key,
+country varchar(45),
+capital varchar(45)
+);
---
+++ World-Flag-Quiz/queries.sql
@@ -0,0 +1,5 @@
+create table flags (
+id serial primary key,
+name varchar(45),
+flag text
+);
